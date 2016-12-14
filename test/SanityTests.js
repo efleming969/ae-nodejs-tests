d66e878ae5d0ff4b5a0384b293852c4d0e0cd9f7
Erick Fleming <efleming969@gmail.com>
2016-12-14 10:34:22 -0700

    basic project structure with api test

--- test/SanityTests.js
+++ test/SanityTests.js
@@ -1,9 +1,17 @@
-var assert = require( "chai" ).assert
+var assert = require("chai").assert
+var agent = require("superagent");
 
 describe( "sanity test", function () {
-  it( "sanity", function () {
-    assert.equal( "foo", "foo" )
-  } );
 
-} );
+  this.timeout(10000);
+
+  it( "sanity post", function ( done ) {
+    agent.post( "http://httpbin.org/post" )
+      .send( {name:"foo"} )
+      .end( function( err, res ) {
+        console.log( JSON.parse( res.body.data ) )
+        done()
+      } )
+  } )
+} )
 
